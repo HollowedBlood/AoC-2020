a4c6e9453cf65d1f301f0c4cfbaf82c7f5031a0e
HollowedBlood <hunterbaugus@gmail.com>
2020-12-06 20:10:35 -0600

    reformat solution 3D1P AoC2020
    I liked making the mountain grow large with the first answer, but thought that if it got too large it would freeze the computer. also it is hard to follow with the eyes.  this update was just a modification to the previous answer that makes it easier to follow and  i suspect less intensive on the computer

---
+++ 3D1P/ProgramSmallform.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace _3D1P
+{
+    class Program
+    {
+        static void Main(string[] _0)
+        {
+            string inputPath = @"E:\Libraries\Documents\GitHub\AoC-2020\3D1P\input.txt";
+            List<string> inputList = File.ReadAllLines(inputPath).ToList();
+            char toboggan = 'T';
+            char collide = 'C';
+            char tree = '#';
+            int collisions = 0;
+            int tobogganX = 0;
+            int tobogganY = 0;
+            StringBuilder mountainLayer = new StringBuilder();
+            foreach (string inputLine in inputList)
+            {
+                mountainLayer.Append(inputLine);
+                if (mountainLayer.Length -1 < tobogganX & tobogganX != 0) //-1 to shift the length into final index value
+                {
+                    tobogganX -= mountainLayer.Length;
+                }
+                
+                if (mountainLayer[tobogganX] == tree)
+                {
+                    mountainLayer[tobogganX] = collide;
+                    collisions += 1;
+                }
+                else
+                {
+                    mountainLayer[tobogganX] = toboggan;
+                }
+                tobogganX += 3;
+                tobogganY += 1;
+                Console.WriteLine(mountainLayer);
+                mountainLayer.Clear();
+            }
+            Console.WriteLine("Collisions: {0}", collisions);
+        }
+    }
+}
