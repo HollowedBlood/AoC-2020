1e7cf5a7a4e9479f53b595b198a8436fab73de5f
HollowedBlood <hunterbaugus@gmail.com>
2020-12-06 19:43:05 -0600

    AoC 3D1P Solved
    completed the day 3 puzzle 1 advent of code 2020, using a stringbuilder that  grew when  needing extra space

---
+++ 3D1P/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace _1D1P
+{
+    class Program
+    {
+        static void Main(string[] _args)
+        {
+            string inputPath = @"E:\Libraries\Documents\GitHub\AoC-2020\3D1P\input.txt";
+            List<string> inputList = File.ReadAllLines(inputPath).ToList();
+            char toboggan = 'T';
+            char collide = 'C';
+            char tree = '#';
+            int collisions = 0;
+            int tobogganX = 0;
+            int tobogganY = 0;
+            StringBuilder mountainLayer = new StringBuilder();
+            foreach (string inputLine in inputList)
+            {
+                while(mountainLayer.Length - 1 <= tobogganX) //-1 to shift length value to final index
+                {
+                    mountainLayer.Append(inputLine);
+                }
+                if (mountainLayer[tobogganX] == tree)
+                {
+                    mountainLayer[tobogganX] = collide;
+                    collisions += 1;
+                }
+                else
+                {
+                    mountainLayer[tobogganX] = toboggan;
+                }
+                tobogganX += 3;
+                tobogganY += 1;
+                Console.WriteLine(mountainLayer);
+                mountainLayer.Clear();
+            }
+            Console.WriteLine("Collisions: {0}", collisions);
+        }
+    }
+}
